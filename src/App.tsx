21deebfb42168c6b43db4c38bb6e894754afcea9
Jsinco <jonah@jsinco.dev>
2025-02-19 21:34:17 -0500

    router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styles from "./App.module.css";
 import Mainpage from "./sections/Mainpage/MainPage";
-import SecondSection from "./sections/SecondSection/SecondSection";
-import ThirdSection from "./sections/ThirdSection/ThirdSection.tsx";
 
 function App() {
     return (
-        <div className={styles.appContainer}>
-            <Mainpage />
-            <SecondSection />
-            <ThirdSection/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={
+                    <div className={styles.appContainer}>
+                        <Mainpage />
+                    </div>
+                }/>
+            </Routes>
+        </Router>
     );
 }
 
--- src/sections/MainPage/MainPage.tsx
+++ src/sections/MainPage/MainPage.tsx
@@ -1,18 +1,24 @@
 import styles from "./MainPage.module.css";
 import logo from "../../assets/LumaLogo.png";
 import IPButton from "../../assets/IPButton.png";
+import SecondSection from "../SecondSection/SecondSection.tsx";
+import ThirdSection from "../ThirdSection/ThirdSection.tsx";
 
 function Mainpage() {
     return (
-        <section className={styles.heroSection}>
-            <section className={styles.logoSection}>
-                <img src={logo} alt="Luma Logo" className={`${styles.logoImage} ${styles.imageHover}`} />
-            </section>
+        <>
+            <section className={styles.heroSection}>
+                <section className={styles.logoSection}>
+                    <img src={logo} alt="Luma Logo" className={`${styles.logoImage} ${styles.imageHover}`}/>
+                </section>
 
-            <section className={styles.IPButtonSection}>
-                <img src={IPButton} alt="IP Button" className={`${styles.IPButtonImg} ${styles.imageHover}`} />
+                <section className={styles.IPButtonSection}>
+                    <img src={IPButton} alt="IP Button" className={`${styles.IPButtonImg} ${styles.imageHover}`}/>
+                </section>
             </section>
-        </section>
+            <SecondSection/>
+            <ThirdSection/>
+        </>
     );
 }
 export default Mainpage;
